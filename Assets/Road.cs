30dc89236d3b7cd4a1b869e810044a9b3b307909
Matthias Gradl <matthias.gradl@gmail.com>
2019-01-25 22:23:30 +0100

    Generated low poly "road"

---
+++ Assets/Road.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+
+    [SerializeField]
+    private float bumpiness = 0.1f;
+
+    [SerializeField]
+    private int tilesWide = 20;
+
+    [SerializeField]
+    private int tilesLong = 100;
+
+    private Vector3[] terrainPoints;
+
+    private Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        float tileWidth = transform.localScale.x / tilesWide;
+        float tileLength = transform.localScale.z / tilesLong;
+
+
+
+    terrainPoints = new Vector3[(tilesWide+1)*(tilesLong+1)];
+        Vector2[] uv = new Vector2[terrainPoints.Length];
+        for (int y=0; y <= tilesLong; y++)
+        {
+            for (int x= 0; x <= tilesWide; x++)
+            {
+                terrainPoints[x + y * (tilesWide+1)] = new Vector3(tileWidth * x, Random.Range(0, bumpiness), tileLength * y);
+                uv[x + y * (tilesWide + 1)] = new Vector2((float) x / tilesWide, (float) y / tilesLong);
+            }
+        }
+
+        mesh = new Mesh();
+        mesh.name = "bumpy road";
+        mesh.vertices = terrainPoints;
+        mesh.uv = uv;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        int[] triangles = new int[tilesLong*tilesWide*6];
+        for (int y = 0; y < tilesLong; y++)
+        {
+            for (int x = 0; x < tilesWide; x++)
+            {
+                triangles[(x + y * tilesWide) * 6 + 0] = (x + 0) + (y + 0) * (tilesWide + 1);
+                triangles[(x + y * tilesWide) * 6 + 1] = (x + 0) + (y + 1) * (tilesWide + 1);
+                triangles[(x + y * tilesWide) * 6 + 2] = (x + 1) + (y + 0) * (tilesWide + 1);
+                triangles[(x + y * tilesWide) * 6 + 3] = (x + 0) + (y + 1) * (tilesWide + 1);
+                triangles[(x + y * tilesWide) * 6 + 4] = (x + 1) + (y + 1) * (tilesWide + 1);
+                triangles[(x + y * tilesWide) * 6 + 5] = (x + 1) + (y + 0) * (tilesWide + 1);
+            }
+        }
+
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (terrainPoints == null)
+            return;
+        Gizmos.color = Color.black;
+        for (int i = 0; i < terrainPoints.Length; i++)
+        {
+            Gizmos.DrawSphere(terrainPoints[i], 0.01f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
