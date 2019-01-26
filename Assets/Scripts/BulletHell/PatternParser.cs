6cab2d7cda37fe072f4f11667b853bc5b0948363
Robert Wetzold <rwetzold@googlemail.com>
2019-01-26 01:26:50 +0100

    Bullet Hell Pattern Parser

---
+++ Assets/Scripts/BulletHell/PatternParser.cs
@@ -0,0 +1,97 @@
+﻿public class PatternParser
+{
+    private string[] pattern = new string[] { };
+    private int currentStep;
+    private int remainingLoops = -1;
+    private int loopCount = 0;
+    private int loopLength = 0;
+    private int loopStart = 0;
+    private int loopStep = 0;
+
+    public PatternParser(string pattern)
+    {
+        if (pattern.Length > 0)
+        {
+            this.pattern = pattern.Split(',');
+        }
+    }
+
+    public int? GetNextExecution()
+    {
+        if (pattern.Length == 0 || currentStep >= pattern.Length)
+        {
+            return null;
+        }
+
+        int idx = GetCalculatedIdx();
+        return int.Parse(pattern[idx].Trim());
+    }
+
+    public string GetNextAction()
+    {
+        if (pattern.Length == 0 || currentStep >= pattern.Length)
+        {
+            return null;
+        }
+
+        int idx = GetCalculatedIdx() + 1;
+        return pattern[idx].Trim();
+    }
+
+    public int? Next()
+    {
+        if (remainingLoops >= 0)
+        {
+            loopStep += 1;
+            if (loopStep >= loopLength)
+            {
+                loopStep = 0;
+                remainingLoops -= 1;
+                if (remainingLoops == 0)
+                {
+                    currentStep += 2;
+                    remainingLoops = -1;
+                }
+            }
+        }
+        else
+        {
+            currentStep += 2;
+        }
+
+        return GetNextExecution();
+    }
+
+    private int GetCalculatedIdx()
+    {
+        string cur = pattern[currentStep].Trim().ToLower();
+        if (remainingLoops <= 0 && cur.StartsWith("loop"))
+        {
+            string[] arr = cur.Split(' ');
+            loopLength = int.Parse(arr[1].Trim());
+
+            string loopCountStr = pattern[currentStep + 1].Trim();
+            if (loopCountStr == "*")
+            {
+                loopCount = int.MaxValue;
+            }
+            else
+            {
+                loopCount = int.Parse(loopCountStr);
+            }
+            loopStep = 0;
+            loopStart = currentStep - 2 * loopLength;
+            remainingLoops = loopCount;
+        }
+
+        if (remainingLoops > 0)
+        {
+            return loopStart + loopStep * 2;
+        }
+        else
+        {
+            return currentStep;
+        }
+
+    }
+}
