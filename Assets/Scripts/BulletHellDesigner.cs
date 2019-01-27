ea99beeb17020d1d063b557134086ab8b1bc304e
Robert Wetzold <rwetzold@googlemail.com>
2019-01-27 13:59:01 +0100

    Wagon Bullet Hell

---
+++ Assets/Scripts/BulletHellDesigner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BulletHellDesigner : MonoBehaviour
+{
+    void Start()
+    {
+        // activate full bullet hell
+        BulletHellManager[] bhm = FindObjectsOfType<BulletHellManager>();
+        foreach (BulletHellManager bh in bhm)
+        {
+            bh.enabled = true;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
